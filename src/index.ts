6d06e7189287b1db38228fa2b9d22eb5f7e4c2c6
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-09-02 21:25:30 +0200

    Update starterkit for v1.82.0-rc.1

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ import { initAdvancedVideoEditor } from './imgly';
  */
 export const DEMO_ASSETS_BASE_URL: string =
   import.meta.env.VITE_DEMO_ASSETS_BASE_URL ||
-  'https://staticimgly.com/imgly/cesdk-web-examples-data/1.81.1/starterkit-advanced-video-editor';
+  'https://staticimgly.com/imgly/cesdk-web-examples-data/1.82.0-rc.1/starterkit-advanced-video-editor';
 
 // ============================================================================
 // Configuration
